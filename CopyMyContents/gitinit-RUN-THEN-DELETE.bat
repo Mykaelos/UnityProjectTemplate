3ee3e8a970b2514b867e5096063664a5a25e816b
Mykaelos <mmaderii@gmail.com>
2020-07-06 14:09:22 -0500

    Created gitinit script to initialize the git repo, add MUL, and make the initial commit. placeholders were added to all of the folders and the new folder layout I'm trying.

---
+++ CopyMyContents/gitinit-RUN-THEN-DELETE.bat
@@ -0,0 +1,16 @@
+@ECHO OFF
+
+ECHO Initializing git project.
+git init
+
+ECHO Adding MykaelosUnityLibrary.
+git submodule add https://github.com/Mykaelos/MykaelosUnityLibrary.git Assets/Libraries/MykaelosUnityLibrary
+
+git add .
+git reset gitinit-RUN-THEN-DELETE.bat
+
+ECHO Making initial commit. 
+git commit -m "Initial commit." -m "Added MykaelosUnityLibrary and all project setup." -m "And so it begins..."
+
+ECHO Project creation finished.
+PAUSE
