572cb98e5791917c1f2479e9649b4f041615ca4f
Mykaelos <mmaderii@gmail.com>
2022-06-14 14:32:15 -0500

    Updated .gitignore to use the latest stuff I learned from Flow. Added some default folder structure to to the Assets folder. Added Android PreloadSigningAlias which I use everytime anyways. Moved documents to a Documents folder.

---
+++ CopyMyContents/Assets/Android/Editor/PreloadSigningAlias.cs
@@ -0,0 +1,15 @@
+﻿//using UnityEditor;
+
+//[InitializeOnLoad]
+//public class PreloadSigningAlias {
+//    static PreloadSigningAlias() {
+//        PlayerSettings.Android.keystorePass = "PASSWORD";
+//        PlayerSettings.Android.keyaliasName = "key";
+//        PlayerSettings.Android.keyaliasPass = "PASSWORD";
+//    }
+//}
+
+//http://answers.unity3d.com/questions/757834/publishing-settings-keystore-password-not-saving.html
+//To use this, copy this file into your editor folder, comment the code back in, and then update the
+//strings to match your passwords and stuff. This allows you to save your strings in git (so you don't
+//forget) and to not have to re-enter them every time you restart Unity and want to push to a device.
